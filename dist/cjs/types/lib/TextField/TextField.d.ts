ee9b1e1bfd5d03ab785ef78edc022d35285a8370
murichristopher <murichristopher@outlook.com>
2024-01-04 19:52:55 -0300

    Remove dist folder

--- dist/cjs/types/lib/TextField/TextField.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { InputHTMLAttributes } from "react";
-import "./TextField.css";
-export interface Props extends InputHTMLAttributes<HTMLInputElement> {
-    label: string;
-}
-declare function TextField(props: Props): React.JSX.Element;
-export default TextField;
